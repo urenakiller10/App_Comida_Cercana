2a2f55e4d5896b368e9f837cd7bbbc4fe26dc541
urenakiller10 <Ub.andrey06105338@gmail.com>
2024-11-06 11:42:26 -0600

    Que la nav bar abra las opciones que son, además se crea pantalla de estadísticas

---
+++ CrowFunding/app/src/main/java/com/example/crowfunding/Estadisticas_Admin.java
@@ -0,0 +1,13 @@
+package com.example.crowfunding;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Estadisticas_Admin extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Inflar el layout correspondiente
+        setContentView(R.layout.view_estadisticas_admin);
+    }
+}
